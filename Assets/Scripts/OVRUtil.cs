538489cfbb36cfcf98fbed8d83995cc14749b2fa
udon <naoki_udon@outlook.jp>
2018-09-11 01:48:43 +0900

    environment

---
+++ Assets/Scripts/OVRUtil.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRUtil : MonoBehaviour
+{
+    private void Start()
+    {
+        OVRManager.display.displayFrequency = 72f;
+
+        OVRManager.cpuLevel = 2;
+        OVRManager.gpuLevel = 2;
+    }
+}
