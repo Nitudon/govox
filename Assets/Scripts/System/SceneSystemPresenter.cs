728b7b1d02aa2456e345e34c886039b49b24861c
udon <naoki_udon@outlook.jp>
2018-09-16 02:50:02 +0900

    scene initializer

---
+++ Assets/Scripts/System/SceneSystemPresenter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UdonLib.Commons;
+
+public class SceneSystemPresenter : UdonBehaviour
+{
+    [SerializeField]
+    private InitializableMono[] _initializers;
+
+    protected override void Start()
+    {
+        base.Start();
+        _initializers.ForEach(x => x.Initialize());
+    }
+}
