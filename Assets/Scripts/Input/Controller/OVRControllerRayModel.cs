feced752e3871607ada5a603b0295d3e3cd4bb12
udon <naoki_udon@outlook.jp>
2018-09-17 01:16:15 +0900

    ray subject model

---
+++ Assets/Scripts/Input/Controller/OVRControllerRayModel.cs
@@ -0,0 +1,18 @@
+﻿using UniRx;
+using UdonLib.Commons;
+using UnityEngine;
+
+public class OVRControllerRayModel : IRayHandler
+{
+    public Subject<RaycastHit> OnRaycastHitObject;
+
+    public OVRControllerRayModel()
+    {
+        OnRaycastHitObject = new Subject<RaycastHit>();
+    }
+
+    public void OnRayHit(RaycastHit hit)
+    {
+        OnRaycastHitObject.OnNext(hit);
+    }
+}
