9ac740d28af8a67c6ba5d88f7f6dd4d755697e87
udon <naoki_udon@outlook.jp>
2018-09-25 02:20:49 +0900

    test ray handle mesh

---
+++ Assets/Scripts/Test/TestRayHandlableCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UdonLib.Commons;
+using UniRx;
+
+public class TestRayHandlableCube : UdonBehaviour, IRayHandler
+{
+    private RayCastMeshSeparateCollision _collision;
+
+    private void Start()
+    {
+        _collision = new RayCastMeshSeparateCollision(CachedTransform);
+
+        _collision.HitDirection.Subscribe(dir => Debug.Log(dir)).AddTo(gameObject);
+    }
+
+    public void OnRayHit(RaycastHit hit)
+    {
+        _collision.OnRayHit(hit);
+    }
+}
---
+++ Assets/Scripts/Test/TestRayHit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonLib.Commons;
+
+public class TestRayHit : UdonBehaviour 
+{
+    private const float RAY_LENGTH = 200f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckRaycastHit();
+        }
+    }
+
+    private void CheckRaycastHit()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var rayHit = Physics.Raycast(ray, out var hit, RAY_LENGTH);
+
+        if(rayHit)
+        {
+            var rayHandler = hit.collider.GetComponent<IRayHandler>();
+
+            if (rayHandler != null)
+            {
+                rayHandler.OnRayHit(hit);
+            }
+        }
+    }
+}
