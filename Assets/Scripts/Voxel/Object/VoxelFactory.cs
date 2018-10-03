2d32eb2a2df7446a5498724a6f31ac284b14a315
udon <naoki_udon@outlook.jp>
2018-10-04 00:04:35 +0900

    create voxel instance factory

---
+++ Assets/Scripts/Voxel/Object/VoxelFactory.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using govox;
+
+public class VoxelFactory
+{
+		private static Dictionary<int, IVoxel> _voxelMap;
+
+	    public static IVoxel Create<T>(T prefab, Transform parent) where T : MonoBehaviour, IVoxel
+        {
+            if(_voxelMap == null)
+            {
+                _voxelMap = new Dictionary<int, IVoxel>();
+            }
+
+            var instance = GameObject.Instantiate(prefab, parent);
+            var id = instance.GetInstanceID();
+            _voxelMap.Add(id, instance);
+
+            return instance;
+        }
+
+        public static bool TryGetVoxel(int id, out IVoxel voxel)
+        {
+            if(_voxelMap == null)
+            {
+                voxel = default(IVoxel);
+                return false;
+            }
+            
+            return _voxelMap.TryGetValue(id, out voxel);
+        }
+
+        public static void Destroy(GameObject instance)
+        {
+            if(_voxelMap != null && _voxelMap.ContainsKey(instance.GetInstanceID()))
+            {
+                _voxelMap.Remove(instance.GetInstanceID());
+            }
+
+            Destroy(instance);
+        }
+}
--- Assets/Scripts/Voxel/Object/VoxelUtility.cs
+++ Assets/Scripts/Voxel/Object/VoxelUtility.cs
@@ -5,26 +5,6 @@ namespace govox
 {
     public class VoxelUtility
     {
-        public static IVoxel Create(IVoxel prefab, Transform parent)
-        {
-            if(_voxelMap == null)
-            {
-                _voxelMap = new Dictionary<int, IVoxel>();
-            }
-
-            var instance = GameObject.Instantiate((MonoBehaviour)prefab, parent);
-            var id = instance.GetInstanceID();
-            var voxel = instance as IVoxel;
-            _voxelMap.Add(id, voxel);
-
-            return voxel;
-        }
-
-        public static void Destroy()
-        {
-            
-        }
-
         public static bool VoxelRayCast(Vector3 origin, Vector3 direction, out VoxelHitInfo info, float maxDistance = float.MaxValue)
         {
             if(_voxelMap == null)
